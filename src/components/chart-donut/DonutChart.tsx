73f3d4781a57078d03b5a9a210d6d6fde92348ec
Marcus Ng <marcusngwj@msn.com>
2025-10-29 21:53:29 +0800

    update chart legend

--- src/components/chart-donut/DonutChart.tsx
+++ src/components/chart-donut/DonutChart.tsx
@@ -1,5 +1,6 @@
 import { Label, Pie, PieChart, ResponsiveContainer, Tooltip } from "recharts";
 import { DonutChartLabel } from "./DonutChartLabel";
+import { LegendItem } from "./DonutChartLegendItem";
 
 type DonutChartProps = React.ComponentProps<typeof Pie> & {
   title: string;
@@ -14,59 +15,71 @@ export const DonutChart = ({
   subtitle,
 }: DonutChartProps) => {
   return (
-    <ResponsiveContainer width="100%" height={400}>
-      <PieChart>
-        <Pie
-          data={data}
-          dataKey={dataKey}
-          nameKey={nameKey}
-          cx="50%"
-          cy="50%"
-          innerRadius="60%"
-          outerRadius="80%"
-          labelLine={{ className: "hidden lg:block" }}
-          label={DonutChartLabel}
-        >
-          <Label
-            content={({ viewBox }) => {
-              if (viewBox && "cx" in viewBox && "cy" in viewBox) {
-                return (
-                  <text
-                    x={viewBox.cx}
-                    y={viewBox.cy}
-                    textAnchor="middle"
-                    dominantBaseline="middle"
-                  >
-                    <tspan
+    <>
+      <ResponsiveContainer width="100%" height={400}>
+        <PieChart>
+          <Pie
+            data={data}
+            dataKey={dataKey}
+            nameKey={nameKey}
+            cx="50%"
+            cy="50%"
+            innerRadius="60%"
+            outerRadius="80%"
+            labelLine={{ className: "hidden lg:block" }}
+            label={DonutChartLabel}
+          >
+            <Label
+              content={({ viewBox }) => {
+                if (viewBox && "cx" in viewBox && "cy" in viewBox) {
+                  return (
+                    <text
                       x={viewBox.cx}
                       y={viewBox.cy}
-                      className="fill-foreground text-3xl font-bold"
+                      textAnchor="middle"
+                      dominantBaseline="middle"
                     >
-                      {title}
-                    </tspan>
-                    <tspan
-                      x={viewBox.cx}
-                      y={(viewBox.cy || 0) + 24}
-                      className="fill-muted-foreground"
-                    >
-                      {subtitle}
-                    </tspan>
-                  </text>
-                );
-              }
+                      <tspan
+                        x={viewBox.cx}
+                        y={viewBox.cy}
+                        className="fill-foreground text-3xl font-bold"
+                      >
+                        {title}
+                      </tspan>
+                      <tspan
+                        x={viewBox.cx}
+                        y={(viewBox.cy || 0) + 24}
+                        className="fill-muted-foreground"
+                      >
+                        {subtitle}
+                      </tspan>
+                    </text>
+                  );
+                }
+              }}
+            />
+          </Pie>
+          <Tooltip
+            formatter={(value: number) =>
+              `$${value.toFixed(2).toLocaleString()}`
+            }
+            contentStyle={{
+              backgroundColor: "var(--background)",
+              border: "var(--border)",
+              borderRadius: "var(--radius)",
             }}
           />
-        </Pie>
-        <Tooltip
-          formatter={(value: number) => `$${value.toFixed(2).toLocaleString()}`}
-          contentStyle={{
-            backgroundColor: "var(--background)",
-            border: "var(--border)",
-            borderRadius: "var(--radius)",
-          }}
-        />
-      </PieChart>
-      <div>Legend</div>
-    </ResponsiveContainer>
+        </PieChart>
+      </ResponsiveContainer>
+      <div className="flex flex-wrap justify-center w-full lg:hidden">
+        {data?.map((item) => (
+          <LegendItem
+            key={item.name as string}
+            name={item.name as string}
+            fill={item.fill as string}
+          />
+        ))}
+      </div>
+    </>
   );
 };
