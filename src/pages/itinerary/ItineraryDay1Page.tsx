482a6fae2a6655cc26df8ba31a2d8818815d8ae9
Marcus Ng <marcusngwj@msn.com>
2025-12-09 21:02:47 +0800

    Add day 1

--- src/pages/itinerary/ItineraryDay1Page.tsx
+++ src/pages/itinerary/ItineraryDay1Page.tsx
@@ -1,8 +1,42 @@
 import { Page } from "@/components/page/Page";
 import { PageContainer } from "@/components/page/PageContainer";
 import { PageContainerItem } from "@/components/page/PageContainerItem";
-// import { PageContainerItemImage } from "@/components/page/PageContainerItemImage";
+import { PageContainerItemImage } from "@/components/page/PageContainerItemImage";
 import { PageHeadline } from "@/components/page/PageHeadline";
+import AirportConnectingTrain from "@/assets/photos/Itinerary_Day1_AirportConnectingTrain.jpg";
+import AirportLinkTrain from "@/assets/photos/Itinerary_Day1_AirportLinkTrain.jpg";
+import Hotel from "@/assets/photos/Itinerary_Day1_Hotel.jpg";
+import HotelBed from "@/assets/photos/Itinerary_Day1_Hotel_Bed.jpg";
+import HotelFoyer from "@/assets/photos/Itinerary_Day1_Hotel_Foyer.jpg";
+import HotelShower from "@/assets/photos/Itinerary_Day1_Hotel_Shower.jpg";
+import HotelToilet from "@/assets/photos/Itinerary_Day1_Hotel_Toilet.jpg";
+import ZhiWeiGuan from "@/assets/photos/Itinerary_Day1_ZhiWeiGuan.jpg";
+import ZhiWeiGuanWefie from "@/assets/photos/Itinerary_Day1_ZhiWeiGuan_Wefie.jpg";
+import ZhiWeiGuanMenu from "@/assets/photos/Itinerary_Day1_ZhiWeiGuan_Menu.jpg";
+import ZhiWeiGuanFood from "@/assets/photos/Itinerary_Day1_ZhiWeiGuan_Food.jpg";
+import ZhiWeiGuanPasteries from "@/assets/photos/Itinerary_Day1_ZhiWeiGuan_Pasteries.jpg";
+import IAmAtHangzhou from "@/assets/photos/Itinerary_Day1_IAmAtHangzhou.jpg";
+import WaterCalligraphy from "@/assets/photos/Itinerary_Day1_WaterCalligraphy.jpg";
+import SausageSnack from "@/assets/photos/Itinerary_Day1_XiHuLiWu_Sausage.jpg";
+import YuJinFang from "@/assets/photos/Itinerary_Day1_YuJinFang.jpg";
+import DragonBoat from "@/assets/photos/Itinerary_Day1_Boat_Dragon.jpg";
+import FruitTea from "@/assets/photos/Itinerary_Day1_FruitTea.jpg";
+import Baidi from "@/assets/photos/Itinerary_Day1_BaiDi.jpg";
+import Tower from "@/assets/photos/Itinerary_Day1_Solo_Tower.jpg";
+import MarblePlaque from "@/assets/photos/Itinerary_Day1_PingHuQiuYue_MarblePlaque.jpg";
+import Totem from "@/assets/photos/Itinerary_Day1_PingHuQiuYue_Totem.jpg";
+import Skewer from "@/assets/photos/Itinerary_Day1_Skewer.jpg";
+import YueWangLang from "@/assets/photos/Itinerary_Day1_YueWangLang.jpg";
+import Hut from "@/assets/photos/Itinerary_Day1_Solo_Hut.jpg";
+import RiverCruise from "@/assets/photos/Itinerary_Day1_Boat_RiverCruise.jpg";
+import River from "@/assets/photos/Itinerary_Day1_Solo_River.jpg";
+import StatueGroup from "@/assets/photos/Itinerary_Day1_Statue_Group.jpg";
+import StatueBarrel from "@/assets/photos/Itinerary_Day1_Statue_Barrel.jpg";
+import StatueWaitress from "@/assets/photos/Itinerary_Day1_Statue_Waitress.jpg";
+import WulinNightMarket from "@/assets/photos/Itinerary_Day1_Wulin_NightMarket.jpg";
+import WulinStory from "@/assets/photos/Itinerary_Day1_Wulin_Story.jpg";
+import WulinStoryFood1 from "@/assets/photos/Itinerary_Day1_Wulin_Story_Food1.jpg";
+import WulinStoryFood2 from "@/assets/photos/Itinerary_Day1_Wulin_Story_Food2.jpg";
 
 export const ItineraryDay1Page = () => {
   return (
@@ -23,6 +57,95 @@ export const ItineraryDay1Page = () => {
           more recently with desktop publishing software like Aldus PageMaker
           including versions of Lorem Ipsum.
         </PageContainerItem>
+        <PageContainerItemImage images={[AirportConnectingTrain]} />
+        <PageContainerItem>
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only
+          five centuries, but also the leap into electronic typesetting,
+          remaining essentially unchanged. It was popularised in the 1960s with
+          the release of Letraset sheets containing Lorem Ipsum passages, and
+          more recently with desktop publishing software like Aldus PageMaker
+          including versions of Lorem Ipsum.
+        </PageContainerItem>
+        <PageContainerItemImage images={[AirportLinkTrain]} />
+        <PageContainerItem>
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only
+          five centuries, but also the leap into electronic typesetting,
+          remaining essentially unchanged. It was popularised in the 1960s with
+          the release of Letraset sheets containing Lorem Ipsum passages, and
+          more recently with desktop publishing software like Aldus PageMaker
+          including versions of Lorem Ipsum.
+        </PageContainerItem>
+        <PageContainerItemImage images={[Hotel]} />
+        <PageContainerItem>
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only
+          five centuries, but also the leap into electronic typesetting,
+          remaining essentially unchanged. It was popularised in the 1960s with
+          the release of Letraset sheets containing Lorem Ipsum passages, and
+          more recently with desktop publishing software like Aldus PageMaker
+          including versions of Lorem Ipsum.
+        </PageContainerItem>
+        <PageContainerItemImage images={[HotelBed, HotelFoyer, HotelShower, HotelToilet]} />
+        <PageContainerItem>
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only
+          five centuries, but also the leap into electronic typesetting,
+          remaining essentially unchanged. It was popularised in the 1960s with
+          the release of Letraset sheets containing Lorem Ipsum passages, and
+          more recently with desktop publishing software like Aldus PageMaker
+          including versions of Lorem Ipsum.
+        </PageContainerItem>
+        <PageContainerItemImage images={[ZhiWeiGuan, ZhiWeiGuanWefie]} />
+        <PageContainerItem>
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only
+          five centuries, but also the leap into electronic typesetting,
+          remaining essentially unchanged. It was popularised in the 1960s with
+          the release of Letraset sheets containing Lorem Ipsum passages, and
+          more recently with desktop publishing software like Aldus PageMaker
+          including versions of Lorem Ipsum.
+        </PageContainerItem>
+        <PageContainerItemImage images={[ZhiWeiGuanMenu, ZhiWeiGuanFood]} />
+        <PageContainerItem>
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only
+          five centuries, but also the leap into electronic typesetting,
+          remaining essentially unchanged. It was popularised in the 1960s with
+          the release of Letraset sheets containing Lorem Ipsum passages, and
+          more recently with desktop publishing software like Aldus PageMaker
+          including versions of Lorem Ipsum.
+        </PageContainerItem>
+        <PageContainerItemImage images={[ZhiWeiGuanPasteries]} />
+        <PageContainerItemImage images={[IAmAtHangzhou]} />
+        <PageContainerItemImage images={[WaterCalligraphy]} />
+        <PageContainerItemImage images={[SausageSnack]} />
+        <PageContainerItemImage images={[YuJinFang]} />
+        <PageContainerItemImage images={[DragonBoat]} />
+        <PageContainerItemImage images={[FruitTea]} />
+        <PageContainerItemImage images={[Baidi]} />
+        <PageContainerItemImage images={[Tower]} />
+        <PageContainerItemImage images={[MarblePlaque, Totem]} />
+        <PageContainerItemImage images={[Skewer]} />
+        <PageContainerItemImage images={[YueWangLang]} />
+        <PageContainerItemImage images={[Hut]} />
+        <PageContainerItemImage images={[RiverCruise, River]} />
+        <PageContainerItemImage images={[StatueGroup, StatueBarrel, StatueWaitress]} />
+        <PageContainerItemImage images={[WulinNightMarket]} />
+        <PageContainerItemImage images={[WulinStory, WulinStoryFood1, WulinStoryFood2]} />
       </PageContainer>
     </Page>
   );
