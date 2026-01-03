3572e4cad68d3b77b48034339143312202a1c3f5
Marcus Ng <marcusngwj@msn.com>
2026-01-03 15:34:09 +0800

    added day 0 text

--- src/pages/itinerary/ItineraryDay0Page.tsx
+++ src/pages/itinerary/ItineraryDay0Page.tsx
@@ -21,15 +21,19 @@ export const ItineraryDay0Page = () => {
     <>
       <Flipboard flight={outboundFlight} />
       <PageContainerItem>
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text ever
-        since the 1500s, when an unknown printer took a galley of type and
-        scrambled it to make a type specimen book. It has survived not only five
-        centuries, but also the leap into electronic typesetting, remaining
-        essentially unchanged. It was popularised in the 1960s with the release
-        of Letraset sheets containing Lorem Ipsum passages, and more recently
-        with desktop publishing software like Aldus PageMaker including versions
-        of Lorem Ipsum.
+        Out of office mode officially activated! This was a highly anticipated
+        trip, though work schedules meant we couldn't lock in our flights until
+        the very last minute. Despite the challenges of late planning and having
+        to work with limited availability, luck was on our side as we snagged an
+        excellent deal. Our adventure finally began at Changi Airport Terminal
+        4.
+      </PageContainerItem>
+      <PageContainerItem>
+        Arriving well ahead of schedule proved to be a wise decision, as we
+        immediately faced a hurdle with our tickets not displaying our full
+        names. Once the airline successfully resolved the issue, we headed to
+        the terminal's Kopitiam to enjoy a familiar meal before clearing the
+        e-gates.
       </PageContainerItem>
       <PageContainerItemMedia
         data={[
@@ -39,17 +43,13 @@ export const ItineraryDay0Page = () => {
             className: "col-span-full max-w-lg mx-auto",
           },
         ]}
+        caption="The non-negotiable first photo of every travel story"
       />
       <PageContainerItem>
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text ever
-        since the 1500s, when an unknown printer took a galley of type and
-        scrambled it to make a type specimen book. It has survived not only five
-        centuries, but also the leap into electronic typesetting, remaining
-        essentially unchanged. It was popularised in the 1960s with the release
-        of Letraset sheets containing Lorem Ipsum passages, and more recently
-        with desktop publishing software like Aldus PageMaker including versions
-        of Lorem Ipsum.
+        Even with a full dinner settled, a visit to the Blossom Lounge was a
+        must. With the year coming to a close, we wanted to utilize the
+        remaining guest visits on our Priority Pass, courtesy of our DBS
+        Altitude Visa Signature card.
       </PageContainerItem>
       <PageContainerItemMedia
         data={[
@@ -61,15 +61,13 @@ export const ItineraryDay0Page = () => {
         ]}
       />
       <PageContainerItem>
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text ever
-        since the 1500s, when an unknown printer took a galley of type and
-        scrambled it to make a type specimen book. It has survived not only five
-        centuries, but also the leap into electronic typesetting, remaining
-        essentially unchanged. It was popularised in the 1960s with the release
-        of Letraset sheets containing Lorem Ipsum passages, and more recently
-        with desktop publishing software like Aldus PageMaker including versions
-        of Lorem Ipsum.
+        Inside, the lounge offered an impressive spread of free-flow options
+        that catered to every craving. We found a variety of hot, freshly cooked
+        dishes alongside a crisp selection of salads and light snacks, perfect
+        for a pre-flight indulgence. For those with a sweet tooth, the ice cream
+        station provided a delightful treat, while the well-stocked beverage
+        corner featured everything from premium coffees and teas to refreshing
+        chilled drinks.
       </PageContainerItem>
       <PageContainerItemMedia
         data={[
@@ -118,15 +116,11 @@ export const ItineraryDay0Page = () => {
         ]}
       />
       <PageContainerItem>
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text ever
-        since the 1500s, when an unknown printer took a galley of type and
-        scrambled it to make a type specimen book. It has survived not only five
-        centuries, but also the leap into electronic typesetting, remaining
-        essentially unchanged. It was popularised in the 1960s with the release
-        of Letraset sheets containing Lorem Ipsum passages, and more recently
-        with desktop publishing software like Aldus PageMaker including versions
-        of Lorem Ipsum.
+        Although the main dining area was buzzing with travelers, we managed to
+        find a quiet corner to settle in. We explored the space and noted that
+        the cozy resting pods were mostly claimed, while the bar area remained
+        peaceful. It became the perfect spot to recharge our devices and relax
+        with some videos before the final stroll to our gate.
       </PageContainerItem>
       <PageContainerItemMedia
         data={[
@@ -141,15 +135,14 @@ export const ItineraryDay0Page = () => {
         ]}
       />
       <PageContainerItem>
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text ever
-        since the 1500s, when an unknown printer took a galley of type and
-        scrambled it to make a type specimen book. It has survived not only five
-        centuries, but also the leap into electronic typesetting, remaining
-        essentially unchanged. It was popularised in the 1960s with the release
-        of Letraset sheets containing Lorem Ipsum passages, and more recently
-        with desktop publishing software like Aldus PageMaker including versions
-        of Lorem Ipsum.
+        For this trip, we flew with Juneyao Air, a full-service carrier that was
+        new to us. Given the last-minute nature of our booking, other airlines
+        had become prohibitively expensive. Juneyao Air sat in the perfect sweet
+        spot, priced only slightly higher than budget options like Spring
+        Airlines but significantly more affordable than Singapore Airlines.
+        Their generous luggage allowance of two 23kg checked bags was a rare and
+        welcome perk that sealed the deal, especially since the departure time
+        fit our itinerary perfectly.
       </PageContainerItem>
       <PageContainerItemMedia
         data={[
@@ -164,15 +157,12 @@ export const ItineraryDay0Page = () => {
         ]}
       />
       <PageContainerItem>
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text ever
-        since the 1500s, when an unknown printer took a galley of type and
-        scrambled it to make a type specimen book. It has survived not only five
-        centuries, but also the leap into electronic typesetting, remaining
-        essentially unchanged. It was popularised in the 1960s with the release
-        of Letraset sheets containing Lorem Ipsum passages, and more recently
-        with desktop publishing software like Aldus PageMaker including versions
-        of Lorem Ipsum.
+        Any initial misconceptions about the airline being budget were quickly
+        dispelled upon boarding. We were pleasantly surprised by the generous
+        legroom and the quality of the seats, where the adjustable headrests
+        provided great support and a mini back cushion added an extra layer of
+        comfort. Between the complimentary supper and the attentive drink
+        service, the hospitality was top-notch.
       </PageContainerItem>
       <PageContainerItemMedia
         data={[
@@ -192,15 +182,11 @@ export const ItineraryDay0Page = () => {
         ]}
       />
       <PageContainerItem>
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text ever
-        since the 1500s, when an unknown printer took a galley of type and
-        scrambled it to make a type specimen book. It has survived not only five
-        centuries, but also the leap into electronic typesetting, remaining
-        essentially unchanged. It was popularised in the 1960s with the release
-        of Letraset sheets containing Lorem Ipsum passages, and more recently
-        with desktop publishing software like Aldus PageMaker including versions
-        of Lorem Ipsum.
+        As the cabin lights dimmed and the hum of the engines settled into a
+        steady rhythm, a sense of calm finally took over. Satisfied with the
+        smooth start to our trip, we leaned back into our cushions and closed
+        our eyes, resting as the plane began its long arc through the night sky
+        toward our descent into China.
       </PageContainerItem>
     </>
   );
