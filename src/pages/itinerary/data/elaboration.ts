7ecdd98d17a1d2ae68d8af4ba3c87c70c0955ee6
Marcus Ng <marcusngwj@msn.com>
2026-04-10 20:03:58 +0800

    update day 3 address

--- src/pages/itinerary/data/elaboration.ts
+++ src/pages/itinerary/data/elaboration.ts
@@ -7,10 +7,28 @@ export const elaboration: Record<string, Elaboration> = {
     addressCn: "杭州市西湖区西湖街道龙井路1号杭州西湖风景名胜区内",
     mapCn: "https://surl.amap.com/aLR4yP41AaMv"
   },
+  BeefBoneSoupNoodles: {
+    title: "Beef Bone Soup Noodles",
+    subtitle: "大骨牛肉汤面",
+    addressCn: "上海市虹口区耀江国际广场东南门东北80米",
+    mapCn: "https://surl.amap.com/hXFbCp4t5Rn"
+  },
   BlossomLounge: {
     title: "BLOSSOM",
     addressEn: "Level 2, Mezzanine, Departure/Transit Lounge, Singapore Changi Airport, Terminal 4",
     mapEn: "https://maps.app.goo.gl/ckAgBcxR52fMqTmQA"
+  },
+  DaHuChun: {
+    title: "Da Hu Chun",
+    subtitle: "大壶春",
+    addressCn: "上海市黄浦区豫园老街64-3号、64-4号、64-5号豫春路21号23号25号27号29号31号",
+    mapCn: "https://surl.amap.com/iO0RUNs1gax6"
+  },
+  GuRenShuo: {
+    title: "Gu Ren Shuo",
+    subtitle: "谷人说",
+    addressCn: "上海市徐汇区叠彩路198号鑫耀光环二期s4-b1-k04a1室",
+    mapCn: "https://surl.amap.com/ipu9BDAG52y"
   },
   HangzhouHotel: {
     title: "Hangzhou Hotel",
@@ -24,6 +42,12 @@ export const elaboration: Record<string, Elaboration> = {
     addressCn: "杭州市上城区河坊街",
     mapCn: "https://surl.amap.com/1CfsXZo1l9Vr"
   },
+  HuXinTing: {
+    title: "Hu Xin Ting",
+    subtitle: "湖心亭",
+    addressCn: "上海市黄浦区豫园老街257号",
+    mapCn: "https://surl.amap.com/iKU2a5kVfGY"
+  },
   LeifengPagoda: {
     title: "Leifeng Pagoda",
     subtitle: "雷峰塔",
@@ -36,12 +60,36 @@ export const elaboration: Record<string, Elaboration> = {
     addressCn: "杭州市拱墅区西湖·D11食尚城南门旁",
     mapCn: "https://surl.amap.com/g2EMtlU1j41b"
   },
+  LuJiaZui: {
+    title: "LuJiaZui",
+    subtitle: "陆家嘴",
+    addressCn: "上海市浦东新区",
+    mapCn: "https://surl.amap.com/4twIs8O1e0DZ"
+  },
+  MehoodElegantHotel: {
+    title: "Mehood Elegant Hotel",
+    subtitle: "上海外滩雅致酒店",
+    addressCn: "上海市虹口区吴淞路299号",
+    mapCn: "https://surl.amap.com/hOVhnSa1h4M3"
+  },
+  MihoyoThemePark: {
+    title: "Mihoyo Theme Park",
+    subtitle: "米哈游主题公园",
+    addressCn: "上海市徐汇区苍梧路与漕宝路交叉口北40米",
+    mapCn: "https://surl.amap.com/iexSDyGD7y9"
+  },
   OmgHeartbeatPark: {
     title: "OMG Heartbeat Park",
     subtitle: "OMG心跳乐园",
     addressEn: "OMG Heartbeat Paradise, Tonglu County, Hangzhou, Zhejiang, China, 311515",
     addressCn: "杭州市桐庐县垂云通天河景区内",
     mapCn: "https://surl.amap.com/gsuhsNY14giA"
+  },
+  OrientalPearlTower: {
+    title: "Oriental Pearl Tower",
+    subtitle: "东方明珠广播电视塔",
+    addressCn: "上海市浦东新区世纪大道1号",
+    mapCn: "https://surl.amap.com/7iUTSjQ1a4DM"
   },
   PingHuQiuYu: {
     title: "Ping Hu Qiu Yu",
@@ -55,6 +103,30 @@ export const elaboration: Record<string, Elaboration> = {
     addressCn: "杭州市西湖区杭州西湖风景名胜区杨公堤38号",
     mapCn: "https://surl.amap.com/4cEHOIy1Abqi"
   },
+  ShanghaiTower: {
+    title: "Shanghai Tower",
+    subtitle: "上海中心大厦",
+    addressCn: "上海市浦东新区东泰路126号",
+    mapCn: "https://surl.amap.com/fb5e4NsHcfo"
+  },
+  ShanghaiWorldFinancialCenter: {
+    title: "Shanghai World Financial Center",
+    subtitle: "上海环球金融中心",
+    addressCn: "上海市浦东新区世纪大道100号",
+    mapCn: "https://surl.amap.com/fedJVvY1gaQ5"
+  },
+  TheBund: {
+    title: "The Bund",
+    subtitle: "外滩",
+    addressCn: "上海市黄浦区中山东二路1号",
+    mapCn: "https://surl.amap.com/DHTfEOadpJ"
+  },
+  TheRingLive: {
+    title: "The Ring Live",
+    subtitle: "鑫耀·光环Live",
+    addressCn: "上海市徐汇区苍梧路6-12号",
+    mapCn: "https://surl.amap.com/ibFQUkqV5kD"
+  },
   WulinFoodStreet: {
     title: "Wulin Food Street",
     subtitle: "武林好吃街",
@@ -67,17 +139,29 @@ export const elaboration: Record<string, Elaboration> = {
     addressCn: "杭州市拱墅区武林街道武林路143号",
     mapCn: "https://surl.amap.com/uW31BU1h9So"
   },
-  YueWangTemple: {
-    title: "Yue Wang Temple",
-    subtitle: "岳王庙",
-    addressCn: "杭州市西湖区北山街80号",
-    mapCn: "https://surl.amap.com/lYjijzM1A0fC"
+  XiaoChiCheng: {
+    title: "Xiao Chi Cheng",
+    subtitle: "小吃城",
+    addressCn: "上海市黄浦区旧校场路15号",
+    mapCn: "https://surl.amap.com/iEBDhGW11K2"
   },
   YueHongJiuJia: {
     title: "Yue Hong Jiu Jia",
     subtitle: "岳虹酒家",
     addressCn: "杭州市西湖区北山街83号",
     mapCn: "https://surl.amap.com/fiUerhE1tdav"
+  },
+  YueWangTemple: {
+    title: "Yue Wang Temple",
+    subtitle: "岳王庙",
+    addressCn: "杭州市西湖区北山街80号",
+    mapCn: "https://surl.amap.com/lYjijzM1A0fC"
+  },
+  YuGarden: {
+    title: "Yu Garden",
+    subtitle: "豫园",
+    addressCn: "上海市黄浦区豫园",
+    mapCn: "https://surl.amap.com/hhDreKqFeSp"
   },
   ZhiWeiGuan: {
     title: "Zhi Wei Guan",
