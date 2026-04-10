1e1795246cd1bb75dc427fc0fb3790f66efc2c7f
Marcus Ng <marcusngwj@msn.com>
2026-04-10 17:55:23 +0800

    Make Dialog more flexible

--- src/components/dialog/DialogBody.tsx
+++ src/components/dialog/DialogBody.tsx
@@ -1,7 +1,8 @@
 import type { ComponentProps } from "react";
+import { cn } from "@/functions/classname";
 
 type DialogBodyProps = ComponentProps<"div">;
 
-export const DialogBody = ({ children }: DialogBodyProps) => {
-  return <div className="p-6">{children}</div>;
+export const DialogBody = ({ children, className }: DialogBodyProps) => {
+  return <div className={cn("p-6", className)}>{children}</div>;
 };
